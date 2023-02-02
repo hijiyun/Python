76f00b3cfce141d3fc80925f2049796e384ec458
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-02-03 01:26:50 +0900

    practice5 커밋

---
+++ practice5.py
@@ -0,0 +1,15 @@
+# num = int(input())
+#
+# i = 0
+# while i < num:
+#     print('',num)
+#     i += 1
+
+#정수를 한 개 입력받아, 1부터 입력받은 수까지 각각에 대해 제곱을 구해 프린트하는 프로그램을 작성해 보세요. 단, while 문을 사용하세요
+
+num = int(input())
+
+i=1
+while i <= num:
+    print(i,i*i)
+    i += 1
